15a850ec1f215b8b43b4f9e689dfb779106ab8d1
Christian Kilb <christian.kilb@live.com>
2014-01-21 12:07:20 +0100

    default images + default image productresult

--- src/classes/results/ProductResult.php
+++ src/classes/results/ProductResult.php
@@ -272,6 +272,64 @@ class ProductResult extends BaseResult
 	}
 	
 	/**
+	 * Returns the URLs for images of the default variant of 
+	 * passed products. If no products are passed, URLs for
+	 * all products will be returneed
+	 * @param int $width width of the images
+	 * @param int $height height of the images
+	 * @see CollinsAPI\\Config::IMAGE_URL
+	 * @return array product urls for default image of each product
+	 */
+	public function getDefaultImageURLs($width = 200, $height = 280)
+	{
+		$urls = array();
+		
+		foreach($this->ids as $product)
+		{
+			if(isset($product['default_image']))
+			{
+				$image = $product['default_image'];
+				$id = $image['id'];
+				$path = substr($id, 0, 3);
+				$extension = $image['extension'];
+
+				$url = str_replace(array(
+					'{{path}}', '{{id}}', '{{extension}}', '{{width}}', '{{height}}'
+				), array(
+					$path, $id, $extension, $width, $height
+				), \CollinsAPI\Config::IMAGE_URL);
+
+				if(!isset($urls[$product['id']]))
+				{
+					$urls[$product['id']] = array();
+				}
+
+				$urls[$product['id']] = $url;
+			}
+		}
+		
+		return $urls;
+	}
+	
+	/**
+	 * Returns the default image URL for a single product
+	 * @param integer $productId ID of the product
+	 * @return string URL of the default image or null if no default image exists
+	 */
+	public function getDefaultImageURL($productId, $width = 200, $height = 280)
+	{
+		$url = null;
+		
+		$urls = $this->getDefaultImageURLs($width, $height);
+		if(isset($urls[$productId]))
+		{
+			$url = $urls[$productId];
+		}
+		
+		return $url;
+	}
+	
+	/**
 	 * Returns all the image URLs for a passed variant
 	 */
 	public function getImageURLsByVariant($productId, $productVariantId, $width = 200, $height = 280)
