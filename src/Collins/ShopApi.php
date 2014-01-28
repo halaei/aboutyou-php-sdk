a8f9e785374aa6d1c86e448bd8ec7d77a0bd0619
Nils Dröge <nils.droege@antevorte.org>
2014-01-28 16:04:02 +0100

    DEVCENTER-53

--- src/Collins/ShopApi.php
+++ src/Collins/ShopApi.php
@@ -1,7 +1,7 @@
 <?php
 namespace Collins;
 
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
+//require_once(__DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
 
 use Collins\ShopApi\Results as Results;
 use Collins\ShopApi\Config;
