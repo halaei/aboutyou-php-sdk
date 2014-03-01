e0fd0a31486bb2918aceeb28cf4d36acb15f3195
Nils Dröge <nils.droege@antevorte.org>
2014-03-01 11:55:25 +0100

    fix tests

--- tests/Functional/ProductGetCategoryTest.php
+++ tests/Functional/ProductGetCategoryTest.php
@@ -44,7 +44,35 @@ class ProductGetCategoryTest extends ShopApiTest
         $this->assertEquals([], $product->getCategoryIds());
     }
 
-    public function testGetCategory()
+    public function testGetCategories()
+    {
+        $product = $this->getProduct('product-with-categories.json');
+        $categories = $product->getCategories();
+        $this->assertInternalType('array', $categories);
+        $this->assertCount(4, $categories);
+        foreach ($categories as $id => $category) {
+            $this->assertInstanceOf('Collins\\ShopApi\\Model\\Category', $category);
+            $this->assertEquals($id, $category->getId());
+        }
+    }
+
+    public function testGetFirstCategory()
+    {
+        $product = $this->getProduct('product-with-categories.json');
+        $category = $product->getFirstCategory();
+        $this->assertInstanceOf('Collins\\ShopApi\\Model\\Category', $category);
+        $this->assertEquals(19080, $category->getId());
+    }
+
+    public function testGetFirstActiveCategory()
+    {
+        $product = $this->getProduct('product-with-categories.json');
+        $category = $product->getFirstActiveCategory();
+        $this->assertInstanceOf('Collins\\ShopApi\\Model\\Category', $category);
+        $this->assertEquals(16080, $category->getId());
+    }
+
+    public function testGetMainCategory()
     {
         $product = $this->getProduct('product-with-categories.json');
         $category = $product->getMainCategory();
