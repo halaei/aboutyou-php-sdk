0999a800b91ae378eae4402bd7b5cee5008da5bf
Christian Kilb <christian.kilb@live.com>
2014-03-11 00:51:49 +0100

    doc

--- src/Collins/ShopApi/Model/BasketItem.php
+++ src/Collins/ShopApi/Model/BasketItem.php
@@ -1,34 +1,113 @@
 <?php
 namespace Collins\ShopApi\Model;
 
+/**
+ * BasketItem is a class used for adding a variant item into the basket
+ *
+ * If you want to add a variant into a basket, you need to create an instance
+ * of a BasketItem. The BasketItem represents a variant by it's variantId.
+ * It can contain $additionalData that will be transmitted to the merchant untouched.
+ * 
+ * Example usage:
+ * $variantId = $variant->getId(); // $variant is instance of \Collins\ShopApi\Model\Variant
+ * $basketItem = new BasketItem($variantId);
+ * $basketItem->setId('my-personal-identifier');
+ * $basketItem->setAdditionalData(['description' => 'jeans with engraving "for you"', 'engraving_text' => 'for you']);
+ * $shopApi->addItemToBasket(session_id(), $basketItem);
+ *
+ * @author  Christian Kilb <christian.kilb@antevorte.org>
+ * @see     \Collins\ShopApi\Model\Variant
+*/
 class BasketItem extends AbstractModel
 {
+    /**
+     * @var int $variantId ID of the Variant
+     */
     protected $variantId;
+    
+    /**
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass a different image URL,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @var array $additionalData additional data for this variant
+     */
     protected $additionalData;
+    
+    /**
+     * The ID of this basket item. You can choose this ID by yourself to identify
+     * your item later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     * 
+     * @var string $id ID of this basket item
+     */
     protected $id;
     
+    /**
+     * 
+     * @param int $variantId ID of the variant
+     * @param array $additionalData additional data for this variant
+     * 
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass a different image URL,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @param string $id ID of the basket item.
+     * 
+     * You can choose this ID by yourself to identify
+     * your item later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     */
     public function __construct($variantId, array $additionalData = array(), $id = null)
     {
         $this->variantId = intval($variantId);
         $this->additionalData = $this->setAdditionalData($additionalData);
-        $this->id = $id ? $id : uniqid();
+        $this->id = $this->setId($id);
     }
     
+    /**
+     * Returns the variant ID of this basket item.
+     * 
+     * @return int
+     */
     public function getVariantId()
     {
         return $this->variantId;
     }
     
+    /**
+     * Sets the variant ID of this basket item.
+     * 
+     * @return int $variant ID of the variant
+     */
     public function setVariantId($variantId)
     {
         $this->variantId = intval($variantId);
     }
     
+    /**
+     * Sets the variant ID of this basket item.
+     * 
+     * @return int $variant ID of the variant
+     */
     public function getAdditionalData()
     {
         return $this->additionalData;
     }
     
+    
+    /** @param array $additionalData additional data for this variant
+     * 
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass a different image URL,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @param array $additionalData
+     * @throws \Collins\ShopApi\Exception\InvalidParameterException
+     */
     public function setAdditionalData(array $additionalData)
     {
         if(count($additionalData) && !isset($additionalData['description'])) {
@@ -40,8 +119,31 @@ class BasketItem extends AbstractModel
         $this->additionalData = $additionalData;
     }
     
+    /**
+     * Returns the ID of this basket item.
+     * 
+     * The ID of this basket item. You can choose this ID by yourself to identify
+     * your item later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     * 
+     * @return string
+     */
     public function getId()
     {
         return $this->id;
+    }
+    
+    /**
+     * Sets the ID of this basket item.
+     * 
+     * The ID of this basket item. You can choose this ID by yourself to identify
+     * your item later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     * 
+     * @param string $id ID of this basket item
+     */
+    public function setId($id)
+    {
+        $this->id = $id ? $id : uniqid();
     }
 }
--- src/Collins/ShopApi/Model/BasketItemSet.php
+++ src/Collins/ShopApi/Model/BasketItemSet.php
@@ -1,15 +1,81 @@
 <?php
 namespace Collins\ShopApi\Model;
 
+
+/**
+ * BasketItemSet is a class used for adding a set of variant items into the basket
+ *
+ * If you want to add a set of variant items into a basket, you need to create an instance
+ * of a BasketItemSet. The BasketItemSet contains BasketItem.
+ * 
+ * A set can be useful if you want to sell several variants as a single product.
+ * For example, if you offer a pair of shoes and additionally different styles of shoelaces
+ * the customer can choose from, you maybe want to put both - shoes and laces - together.
+
+ * 
+ * Example usage:
+ * $lacesVariantId = $lacesVariant->getId(); // $lacesVariant is instance of \Collins\ShopApi\Model\Variant
+ * $shoesVariantID = $shoesVariant->getId(); // $lacesVariant is instance of \Collins\ShopApi\Model\Variant
+ * $basketItem1 = new BasketItem($lacesVariantId);
+ * $basketItem2 = new BasketItem($shoesVariantId);
+ * 
+ * $basketItemSet = new BasketItemSet([$basketItem1, $basketItem2]);
+ * $basketItemSet->setAddition
+ * $basketItemSet->setAdditionalData(['description' => 'Shoes with laces "yellow star"', 'image_url' = 'http://myapp.com/shoes_yello_star.png']);
+ * $shopApi->addItemSetToBasket(session_id(), $basketItemSet);
+ *
+ * @author  Christian Kilb <christian.kilb@antevorte.org>
+ * @see     \Collins\ShopApi\Model\BasketItem
+ * @see     \Collins\ShopApi\Model\Variant
+*/
 class BasketItemSet
 {
+    /**
+     * The ID of this basket item. You can choose this ID by yourself to identify
+     * your item later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     * 
+     * @var string $id ID of this basket item
+     */
     protected $id;
+    
+    /**
+     * @var BasketItem[] array of BasketItems
+     */
     protected $basketItems;
+    
+    /**
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass an image URL that
+     * represents this item set,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @var array $additionalData additional data for this item set
+     */
     protected $additionalData;
     
+    /**
+     * 
+     * @param int $variantId ID of the variant
+     * @param array $additionalData additional data for this item set
+     * 
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass an image URL that
+     * represents this item set,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @param string $id ID of the basket item set.
+     * 
+     * You can choose this ID by yourself to identify
+     * your item set later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     */
     public function __construct(array $basketItems, array $additionalData = array(), $id = null)
     {
-        $this->id = $id ? $id : uniqid();
+        
+        $this->id = $this->setId($id);
         $this->additionalData = $this->setAdditionalData($additionalData);
         
         foreach($basketItems as $basketItem) {
@@ -18,6 +84,11 @@ class BasketItemSet
         
     }
     
+    /**
+     * Adds a basket item to this set.
+     * 
+     * @param \Collins\ShopApi\Model\BasketItem $basketItem
+     */
     public function addBasketItem(BasketItem $basketItem) {
         $prefix = $basketItem->getId();
         $id = $prefix;
@@ -28,6 +99,12 @@ class BasketItemSet
         $this->basketItems[$id] = $basketItem;
     }
     
+    /**
+     * Removes a basket item from this set by it's ID.
+     * 
+     * @param type $id ID of the basket item
+     * @return boolean true if item found and removed
+     */
     public function removeBasketItem($id)
     {
         if(isset($this->basketItems[$id])) {
@@ -38,11 +115,26 @@ class BasketItemSet
         return false;
     }
     
+    /**
+     * Returns all the basket items of this set
+     * 
+     * @return BasketItem[]
+     */
     public function getItems()
     {
         return $this->basketItems;
     }
     
+    /**
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass an image URL that
+     * represents this item set,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @param array $additionalData additional data for this item set
+     * @throws \Collins\ShopApi\Exception\InvalidParameterException
+     */
     public function setAdditionalData(array $additionalData)
     {
         if(count($additionalData) && !isset($additionalData['description'])) {
@@ -55,11 +147,39 @@ class BasketItemSet
         $this->additionalData = $additionalData;
     }
     
+    /**
+     * Returns the ID of this basket item set.
+     * 
+     * @return string
+     */
     public function getId()
     {
         return $this->id;
     }
     
+    /**
+     * Sets the ID of this basket item.
+     * 
+     * The ID of this basket item. You can choose this ID by yourself to identify
+     * your item later.
+     * If you don't pass any ID ($id = null), uniquid() will be used.
+     * 
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id ? $id : uniqid();
+    }
+    
+    /**
+     * Additional data are transmitted to the merchant untouched.
+     * If set (array not empty), a key "description" must exist. This description
+     * must be a string that describes the variant. If you want to pass an image URL that
+     * represents this item set,
+     * you can add a key "image_url" to the $additionalData that contains the URL to the image.
+     * 
+     * @return array additional data
+     */
     public function getAdditionalData()
     {
         return $this->additionalData;
