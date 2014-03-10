d57b6e4b224c3e219435480e5e034e8eef08262d
Nils Dröge <nils.droege@antevorte.org>
2014-03-10 19:16:41 +0100

    enhance order test

--- src/Collins/ShopApi/Model/InitiateOrder.php
+++ src/Collins/ShopApi/Model/InitiateOrder.php
@@ -48,4 +48,32 @@ class InitiateOrder
     {
         return $this->appToken;
     }
+
+    /** @var integer */
+    protected $errorCode;
+
+    /** @var string */
+    protected $errorMessage;
+
+    protected function parseErrorResult(\stdClass $jsonObject)
+    {
+        $this->errorCode    = isset($jsonObject->error_code) ? $jsonObject->error_code : 0;
+        $this->errorMessage = isset($jsonObject->error_message) ? $jsonObject->error_message : null;
+    }
+
+    /**
+     * @return int
+     */
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
 }
--- tests/Functional/OrderTest.php
+++ tests/Functional/OrderTest.php
@@ -59,8 +59,46 @@ class OrderTestAbstract extends AbstractShopApiTest
         $this->markTestIncomplete('implement me');
     }
 
-    public function testInitiateOrderFailed()
+    public function testInitiateOrderFailedWithEmptyBasket()
     {
-        $this->markTestIncomplete('implement me');
+        $shopApi = $this->getShopApiWithResult('[
+            {
+                "initiate_order": {
+                    "error_ident": "440db3b3-75c4-4223-b5cf-e57d37616239",
+                    "error_message": [
+                        "Basket is empty: abcabcabc"
+                    ],
+                    "error_code": 400
+                }
+            }
+        ]');
+        $initiateOrder = $shopApi->initiateOrder(
+            "abcabcabc",
+            "http://somedomain.com/url"
+        );
+        $this->markTestIncomplete('implement behavior');
+
+        $this->assertInstanceOf('Collins\\ShopApi\\Model\\InitiateOrder', $initiateOrder);
+    }
+
+    public function testInitiateOrderFailedWithError()
+    {
+        $response = <<<EOS
+        [{
+            "initiate_order": {
+                "error_message": [ "success_url: u'/checkout/success' does not match '^http(s|)://'" ],
+                "error_code": 400
+            }
+        }]
+EOS;
+
+        $shopApi = $this->getShopApiWithResult($response);
+        $initiateOrder = $shopApi->initiateOrder(
+            "abcabcabc",
+            "/somedomain.com/url"
+        );
+        $this->markTestIncomplete('implent be');
+
+        $this->assertInstanceOf('Collins\\ShopApi\\Model\\InitiateOrder', $initiateOrder);
     }
 }
