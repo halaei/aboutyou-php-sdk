adba9206a311052ea58359676aa57435b12bcc85
Florian Weigang <Florian.Weigang@project-collins.com>
2014-04-08 18:00:17 +0200

    DC-206: Test ProductSearch and Basket with real life products

---
+++ tests/Live/ProductSearchTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Collins\ShopApi\Test\Live;
+
+class ProductSearchTest extends \Collins\ShopApi\Test\Live\AbstractShopApiLiveTest
+{
+    
+    public function testProductSearchWithLimit()
+    {
+        $api = $this->getShopApi();        
+        $criteria = $this->getSearchCriteria();
+        $criteria->setLimit(5);
+        
+        $result = $api->fetchProductSearch($criteria);
+        
+        $this->assertInstanceOf('\Collins\ShopApi\Model\ProductSearchResult', $result);         
+        $this->assertCount(5, $result->getProducts()); 
+        
+        foreach($result->getProducts() as $product) {
+            $this->assertInstanceOf('\Collins\ShopApi\Model\Product', $product);
+        }
+    }
+    
+    public function testProductSearchWithId()
+    {
+        $api = $this->getShopApi();        
+        $criteria = $this->getSearchCriteria();
+        $criteria->setLimit(5);
+        
+        $result = $api->fetchProductSearch($criteria);
+        
+        $productsArray = $result->getProducts();
+        $product = $productsArray[0];
+        
+        $this->assertInstanceOf('\Collins\ShopApi\Model\Product', $product);         
+        
+        $resultProduct = $api->fetchProductsByIds(array($product->getId()));
+        
+        $this->assertInstanceOf('\Collins\ShopApi\Model\ProductsResult', $resultProduct); 
+    }
+    
+    public function testProductSearchCategoryTree()
+    {
+        $api = $this->getShopApi();        
+        $criteria = $this->getSearchCriteria();
+        $criteria->setLimit(5);
+        
+        $result = $api->fetchProductSearch($criteria);
+
+        $this->assertInternalType('array', $result->getCategoryTree());
+    }
+    
+    public function testProductSearchCategory()
+    {
+        $api = $this->getShopApi();        
+        $criteria = $this->getSearchCriteria();
+        $criteria->setLimit(5);
+        
+        $result = $api->fetchProductSearch($criteria);
+
+        $this->assertInternalType('array', $result->getCategories());
+    }    
+    
+    public function testProductSearchProductCount()
+    {
+        $api = $this->getShopApi();        
+        $criteria = $this->getSearchCriteria();
+        $criteria->setLimit(5);
+        
+        $result = $api->fetchProductSearch($criteria);
+
+        $this->assertInternalType('int', $result->getProductCount());
+    }    
+    
+}
+
