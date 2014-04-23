491492ab3c641109286dbc7c757e359290db000e
Nils Dröge <nils.droege@antevorte.org>
2014-04-23 17:20:18 +0200

    naming: changed antevorte to collins

--- src/Collins/ShopApi/Model/Basket/AbstractBasketItem.php
+++ src/Collins/ShopApi/Model/Basket/AbstractBasketItem.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @auther nils.droege@antevorte.org
- * (c) Antevorte GmbH & Co KG
+ * @author nils.droege@project-collins.com
+ * (c) Collins GmbH & Co KG
  */
 
 namespace Collins\ShopApi\Model\Basket;
