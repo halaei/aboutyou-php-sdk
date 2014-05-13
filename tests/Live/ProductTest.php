e1040f3ab9e32e63e784c0722cc0c52b5de3ae73
Nils Dröge <nils.droege@antevorte.org>
2014-05-13 10:01:25 +0200

    added uncomplete getlength test

--- tests/Live/ProductTest.php
+++ tests/Live/ProductTest.php
@@ -2,6 +2,8 @@
 
 namespace Collins\ShopApi\Test\Live;
 
+use Collins\ShopApi\Criteria\ProductFields;
+
 class ProductTest extends \Collins\ShopApi\Test\Live\AbstractShopApiLiveTest
 {
     /**
@@ -13,4 +15,18 @@ class ProductTest extends \Collins\ShopApi\Test\Live\AbstractShopApiLiveTest
         
         $this->assertInternalType('array', $product->getRootCategories());
     }
+
+//      TODO: search for a product with no length, eg. shows
+//    public function testGetVariantGetLength()
+//    {
+//        $id = 402573;
+//        $api = $this->getShopApi();
+//
+//        $productsResult = $api->fetchProductsByIds([$id], array(ProductFields::VARIANTS));
+//        $product = $productsResult[$id];
+//        $variants = $product->getVariants();
+//        $variant = reset($variants);
+//
+//        $this->assertNull($variant->getLength());
+//    }
 }
