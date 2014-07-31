15a2a874c1c9667d9ed2ed5b8a254074a682a64d
Christian Kilb <christian.kilb@project-collins.com>
2014-07-31 12:41:00 +0200

    Variant::getQuantityPerPack implemented

--- src/Collins/ShopApi/Constants.php
+++ src/Collins/ShopApi/Constants.php
@@ -31,6 +31,7 @@ abstract class Constants
     const FACET_CLOTHING_MEN_INCH = 189;
     const FACET_CLOTHING_MEN_DE = 187;
     const FACET_CONDITION = 234;
+    const FACET_QUANTITY_PER_PACK = 263;
 
     const SORT_RELEVANCE = 'relevance';
     const SORT_UPDATED = 'updated_date';
