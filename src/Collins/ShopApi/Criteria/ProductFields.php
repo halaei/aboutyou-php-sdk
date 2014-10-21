6673fd20eb5de82e858d9ec3cac8a5f3d4293f36
Nils Droege <nils.droege@antevorte.org>
2014-10-21 10:22:39 +0200

    DC-935 formating

--- src/Collins/ShopApi/Criteria/ProductFields.php
+++ src/Collins/ShopApi/Criteria/ProductFields.php
@@ -12,22 +12,22 @@ class ProductFields
     /*
      * id and name is set per default
      */
-    const IS_ACTIVE              = "active";
-    const BRAND                  = "brand_id";
-    const DESCRIPTION_LONG       = "description_long";
-    const DESCRIPTION_SHORT      = "description_short";
-    const DEFAULT_VARIANT        = "default_variant";
-    const VARIANTS               = "variants";
-    const MIN_PRICE              = "min_price";
-    const MAX_PRICE              = "max_price";
-    const IS_SALE                = "sale";
-    const DEFAULT_IMAGE          = "default_image";
-    const ATTRIBUTES_MERGED      = "attributes_merged";
-    const CATEGORIES             = "categories";
-    const INACTIVE_VARIANTS      = "inactive_variants";
-    const MAX_SAVINGS            = "max_savings";
-    const MAX_SAVINGS_PERCENTAGE = "max_savings_percentage";
-    const tags                   = "tags";
+    const IS_ACTIVE              = 'active';
+    const BRAND                  = 'brand_id';
+    const DESCRIPTION_LONG       = 'description_long';
+    const DESCRIPTION_SHORT      = 'description_short';
+    const DEFAULT_VARIANT        = 'default_variant';
+    const VARIANTS               = 'variants';
+    const MIN_PRICE              = 'min_price';
+    const MAX_PRICE              = 'max_price';
+    const IS_SALE                = 'sale';
+    const DEFAULT_IMAGE          = 'default_image';
+    const ATTRIBUTES_MERGED      = 'attributes_merged';
+    const CATEGORIES             = 'categories';
+    const INACTIVE_VARIANTS      = 'inactive_variants';
+    const MAX_SAVINGS            = 'max_savings';
+    const MAX_SAVINGS_PERCENTAGE = 'max_savings_percentage';
+    const TAGS                   = 'tags';
 
     public static function filterFields(array $fields)
     {
