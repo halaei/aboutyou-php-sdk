a43a206281c0d8e693a0d8bc639d7c4e558c8ba1
patrick.michelberger <pmichelberger@gmx.de>
2015-01-21 16:19:28 +0100

    DC-1132: fixed

--- src/AboutYou/SDK/Model/CategoryManager/DefaultCategoryManager.php
+++ src/AboutYou/SDK/Model/CategoryManager/DefaultCategoryManager.php
@@ -26,12 +26,12 @@ class DefaultCategoryManager implements CategoryManagerInterface
     private $cacheKey;
 
     /**
-     * @param string $appId  This must set, when you use more then one instances with different apps
+     * @param string $appId This must set, when you use more then one instances with different apps
      * @param CacheProvider $cache
      */
     public function __construct(CacheProvider $cache = null, $appId = '')
     {
-        $this->cache    = $cache;
+        $this->cache = $cache;
         $this->cacheKey = 'AY:SDK:' . $appId . ':categories';
 
         $this->loadCachedCategories();
@@ -67,11 +67,13 @@ class DefaultCategoryManager implements CategoryManagerInterface
     public function parseJson($jsonObject, ModelFactoryInterface $factory)
     {
         $this->categories = array();
-        // this hack converts the array keys to integers, otherwise $this->parentChildIds[$id] fails
-        $this->parentChildIds = json_decode(json_encode($jsonObject->parent_child), true);
+        if (property_exists($jsonObject, parent_child)) {
+            // this hack converts the array keys to integers, otherwise $this->parentChildIds[$id] fails
+            $this->parentChildIds = json_decode(json_encode($jsonObject->parent_child), true);
 
-        foreach ($jsonObject->ids as $id => $jsonCategory) {
-            $this->categories[$id] = $factory->createCategory($jsonCategory, $this);
+            foreach ($jsonObject->ids as $id => $jsonCategory) {
+                $this->categories[$id] = $factory->createCategory($jsonCategory, $this);
+            }
         }
 
         $this->cacheCategories();
