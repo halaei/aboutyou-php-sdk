b81b464115f5a92bca498cb53b745cafe5b4ad2d
Holger Reinhardt <holger.reinhardt@aboutyou.de>
2015-04-14 09:41:04 +0200

    AYD-6595 refactored some parts

--- src/AboutYou/SDK/Model/Product.php
+++ src/AboutYou/SDK/Model/Product.php
@@ -10,6 +10,7 @@ use AboutYou\SDK\Constants;
 use AboutYou\SDK\Exception\MalformedJsonException;
 use AboutYou\SDK\Exception\RuntimeException;
 use AboutYou\SDK\Factory\ModelFactoryInterface;
+use DateTime;
 
 class Product
 {
@@ -52,7 +53,6 @@ class Product
     /** @var DateTime */
     protected $firstPublicationDate;
 
-
     /** @var array */
     protected $categoryIdPaths;
 
@@ -71,27 +71,23 @@ class Product
     /** @var integer[] */
     protected $facetIds;
 
-    /** @var Image */
-    protected $defaultImage;
+    /** @var string */
+    protected $defaultImageHash;
 
-    /**
-     * @var Image
-     */
+    /** @var Image */
     protected $selectedImage;
 
-    /**
-     * @var Image[]
-     */
+    /** @var Image[] */
     protected $images = [];
 
-    /** @var Variant */
-    protected $defaultVariant;
+    /** @var int */
+    protected $defaultVariantId;
 
     /** @var Variant */
     protected $selectedVariant;
 
     /** @var Variant[] */
-    protected $variants;
+    protected $variants = [];
 
     /** @var Variant[] */
     protected $inactiveVariants;
@@ -158,24 +154,18 @@ class Product
         if (isset($jsonObject->default_image)) {
             if (!isset($product->images[$jsonObject->default_image->hash])) {
                 $defaultImage = $factory->createImage($jsonObject->default_image);
-                $product->images = array_merge(
-                    [$defaultImage->getHash() => $defaultImage],
-                    $product->images
-                );
+                $product->images = array_merge([$defaultImage->getHash() => $defaultImage], $product->images);
             }
-            $product->defaultImage = $product->getImageByHash($jsonObject->default_image->hash);
+            $product->defaultImageHash = $jsonObject->default_image->hash;
         }
 
         $product->variants = self::parseVariants($jsonObject, $factory, $product);
         if (isset($jsonObject->default_variant)) {
             if (!isset($product->variants[$jsonObject->default_variant->id])) {
                 $defaultVariant = $factory->createVariant($jsonObject->default_variant, $product);
-                $product->variants = array_merge(
-                    [$defaultVariant->getId() => $defaultVariant],
-                    $product->variants
-                );
+                $product->variants = array_merge([$defaultVariant->getId() => $defaultVariant], $product->variants);
             }
-            $product->defaultVariant = $product->getVariantById($jsonObject->default_variant->id);
+            $product->defaultVariantId = $jsonObject->default_variant->id;
         }
 
         $product->inactiveVariants = self::parseVariants($jsonObject, $factory, $product, 'inactive_variants');
@@ -678,16 +668,7 @@ class Product
      */
     public function getDefaultImage()
     {
-        return $this->defaultImage;
-    }
-
-    public function selectImage($hash)
-    {
-        if ($hash) {
-            $this->selectedImage = $this->getImageByHash($hash);
-        } else {
-            $this->selectedImage = null;
-        }
+        return $this->getImageByHash($this->defaultImageHash);
     }
 
     /**
@@ -703,7 +684,7 @@ class Product
      */
     public function getImage()
     {
-        return $this->selectedImage ?: $this->defaultImage ?: null;
+        return $this->selectedImage ?: $this->getDefaultImage() ?: null;
     }
 
     /**
@@ -718,6 +699,18 @@ class Product
         }
 
         return null;
+    }
+
+    /**
+     * @param string $hash
+     */
+    public function selectImage($hash)
+    {
+        if ($hash) {
+            $this->selectedImage = $this->getImageByHash($hash);
+        } else {
+            $this->selectedImage = null;
+        }
     }
 
     /**
@@ -741,7 +734,7 @@ class Product
      */
     public function getDefaultVariant()
     {
-        return $this->defaultVariant;
+        return $this->getVariantById($this->defaultVariantId);
     }
 
     /**
