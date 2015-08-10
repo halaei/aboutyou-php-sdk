9c8db25403dabadca91ead2c1342368dbc9b5c63
Manuel Piesold <manuel.piesold@aboutyou.de>
2015-08-10 15:20:12 +0200

    added holiday/weekend information to DeliveryEstimation

--- src/AboutYou/SDK/Model/Basket/DeliveryEstimation.php
+++ src/AboutYou/SDK/Model/Basket/DeliveryEstimation.php
@@ -23,22 +23,44 @@ class DeliveryEstimation
      */
     protected $maxDate;
 
+    /**
+     * @var boolean
+     */
+    protected $holiday;
+
+    /**
+     * @var boolean
+     */
+    protected $weekend;
+
+    /**
+     * @var boolean
+     */
+    protected $crossdocked;
+
     public static function createFromJSON($jsonObject)
     {
         return new DeliveryEstimation(
             isset($jsonObject->min) ? intval($jsonObject->min) : null,
             isset($jsonObject->max) ? intval($jsonObject->max) : null,
             isset($jsonObject->min_date) ? new \DateTime($jsonObject->min_date) : null,
-            isset($jsonObject->max_date) ? new \DateTime($jsonObject->max_date) : null
+            isset($jsonObject->max_date) ? new \DateTime($jsonObject->max_date) : null,
+            isset($jsonObject->crossdocked) ? boolval($jsonObject->crossdocked) : null,
+            isset($jsonObject->holiday) ? boolval($jsonObject->holiday) : null,
+            isset($jsonObject->weekend) ? boolval($jsonObject->weekend) : null
+
         );
     }
 
-    public function __construct($minDays, $maxDays, $minDate, $maxDate)
+    public function __construct($minDays, $maxDays, $minDate, $maxDate, $crossdocked, $holiday, $weekend)
     {
         $this->minDays = $minDays;
         $this->maxDays = $maxDays;
         $this->minDate = $minDate;
         $this->maxDate = $maxDate;
+        $this->crossdocked = $crossdocked;
+        $this->holiday = $holiday;
+        $this->weekend = $weekend;
     }
 
     /**
@@ -71,5 +93,29 @@ class DeliveryEstimation
     public function getMaxDate()
     {
         return $this->maxDate;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getHoliday()
+    {
+        return $this->holiday;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getWeekend()
+    {
+        return $this->weekend;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getCrossdocked()
+    {
+        return $this->crossdocked;
     }
 }
